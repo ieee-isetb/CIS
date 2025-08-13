8d5c8fd7006285e9b828f956e5f585a11b628bc0
Allani Mohamed <mohamedallani70@gmail.com>
2025-08-13 11:03:53 +0100

    Project Init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='text-xl bg-red-400'>3aslema</div>
+    </>
+  )
+}
+
+export default App
