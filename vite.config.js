a00586c52fe15a35ca1781209b561f56696d9952
Mohamed Yassine Zorgui <146244180+yassinezorgui@users.noreply.github.com>
2025-11-11 13:10:53 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/CIS/',
+  base: '/', // <-- Change to root for the new domain
   server: {
     host: true
   }
